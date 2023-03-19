1d125761c3c16fdfb9dd6bfc13e240dbe69b8590
Bruno Peixoto <brunolnetto@gmail.com>
2023-03-19 16:35:43 -0300

    feat/ examples refactor

--- examples/index.js
+++ examples/index.js
@@ -1,14 +1,19 @@
 const rtc = require("retoric");
 const { Premise, Conjunction, Disjunction } = rtc;
 
-
-const premise = new Premise("true_premise", "This is a true premise", true);
-const disjunction = new Disjunction_(injprops.key, injprops.description, injprops.value);
-const conjunction = new Conjunction_(conjprops.key, conjprops.description, conjprops.value);
-
 const talk = (description, reason, callback) => {
     console.log(`This is ${description}: ${callback(reason)}`);
 };
+
+const descriptionCallback = (name) => `This is a ${name}`;
+
+const DisConjProps = (type, premises) => {
+    return {
+        key: `${type}`,
+        description: descriptionCallback(type),
+        value: premises,
+    };
+}
 
 const argue = (reason) => talk('argumentation', reason, (reason) => reason.argue());
 const conclude = (reason) => talk('conclusion', reason, (reason) => reason.conclude());
@@ -16,8 +21,45 @@ const summarize = (reason) => talk('summary', reason, (reason) => reason.summari
 const pitch = (reason) => talk('pitch', reason, (reason) => reason.pitch());
 const verbalize = (reason) => talk('verbalization', reason, (reason) => reason.verbalize());
 
-argue(premise);
-conclude(premise);
-summarize(premise);
-pitch(premise);
-verbalize(premise);
+const expose = (reason) => {
+    console.log('------------------------');
+    console.log(reason.description);
+    console.log("\n");
+    argue(reason);
+    conclude(reason);
+    summarize(reason);
+    pitch(reason);
+    verbalize(reason);
+    console.log('------------------------');
+};
+
+const truePremise = new Premise("true_premise", "This is a true premise", true);
+const falsePremise = new Premise("false_premise", "This is a false premise", false);
+
+const premiseArtifacts = [
+  [truePremise, truePremise],
+  [truePremise, falsePremise],
+  [falsePremise, truePremise],
+  [falsePremise, falsePremise],
+];
+
+let props
+let disjunctions = []; 
+let conjunctions = [];
+
+for (const premises of premiseArtifacts) {
+    props = DisConjProps('disjunction', premises);
+    disjunction = new Disjunction(props.key, props.description, props.value);
+    
+    props = DisConjProps('conjunction', premises);
+    conjunction = new Conjunction(props.key, props.description, props.value);
+
+    disjunctions.push(disjunction);
+    conjunctions.push(conjunction);
+}
+
+expose(truePremise);
+expose(falsePremise);
+
+disjunctions.forEach(disjunction => expose(disjunction)); 
+conjunctions.forEach(conjunction => expose(conjunction)); 
